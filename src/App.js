0fe789b5cdc9ac035a022ae68e2f626c4b2ce080
Christianna Xu <christianna.xu@berkeley.edu>
2019-06-29 23:32:34 -0700

    v0.1: starting working on top nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import Toolbar from './components/Toolbar/Toolbar';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
+      <Toolbar />
+      <main style={{marginTop: '64px'}}>
+        <p>This is the page content!</p>
+      </main>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,6 +26,19 @@ function App() {
       </header>
     </div>
   );
+}*/
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Toolbar />
+        <main style={{marginTop: '64px'}}>
+        <p>This is the page content!</p>
+      </main>
+      </div>
+    )
+  }
 }
 
 export default App;
