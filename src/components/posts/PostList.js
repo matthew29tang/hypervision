3c0d9f0afb412867f2d1ec705ae516ea7f1097a7
Christianna Xu <christianna.xu@berkeley.edu>
2019-07-20 20:59:55 -0700

    v0.9 Finished results page

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import PostData from '../data/sampledata.json';
+import { Route, Link } from 'react-router-dom';
 
 class PostList extends Component {
+
     render () {
         return (
             <div >
                 {PostData.map((postDetail, index) => {
                     return <div>
-                        <h3>Link: {postDetail.link}</h3>
-                        <p>Click here to open URL</p>
+                        <h3>Link: <a target="_blank" href={postDetail.link}> {postDetail.link}</a></h3>
                     </div>
                 })}
             </div>
