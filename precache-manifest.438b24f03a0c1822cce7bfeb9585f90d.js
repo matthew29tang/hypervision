19a542ec0f0d80ac9b07708e5aa8c68ef98ab63d
matthew29tang <matthew29tang@gmail.com>
2019-08-12 21:38:15 -0700

    Updates

---
+++ precache-manifest.438b24f03a0c1822cce7bfeb9585f90d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebde3a9d21bcee45a24f868b028086e",
+    "url": "/hypervision/index.html"
+  },
+  {
+    "revision": "0f022cdeea74d3f9728b",
+    "url": "/hypervision/static/css/main.57d3ad94.chunk.css"
+  },
+  {
+    "revision": "53f626e8cf0c4801ec08",
+    "url": "/hypervision/static/js/2.57951583.chunk.js"
+  },
+  {
+    "revision": "0f022cdeea74d3f9728b",
+    "url": "/hypervision/static/js/main.d7d36676.chunk.js"
+  },
+  {
+    "revision": "3efd3c0b3f7c46c3b4bd",
+    "url": "/hypervision/static/js/runtime~main.6e225ed8.js"
+  },
+  {
+    "revision": "3f7e59ccba886ccd5a4c11fb4c47a5e8",
+    "url": "/hypervision/static/media/dropzone.3f7e59cc.svg"
+  },
+  {
+    "revision": "bb0534c8267fd676108f520f60d1d92f",
+    "url": "/hypervision/static/media/progress.bb0534c8.svg"
+  }
+]);
